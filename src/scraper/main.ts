e9e78147c715d83e1b2c0023a865415a47e60127
Jayslen <jayslen134@gmail.com>
2025-11-22 22:54:23 -0400

    update: change the command structure for implemeneting the command to insert the values in DB

--- src/scraper/main.ts
+++ src/scraper/main.ts
@@ -1,5 +1,5 @@
 import inquirer from 'inquirer'
-import { Command, program } from 'commander'
+import { program } from 'commander'
 import { Commands } from './commands/Commander.js'
 import { parseAnswers } from './utils/parseAnswers.js'
 import { LEAGUES_AVAILABLE } from './config.js'
@@ -15,46 +15,68 @@ program
 console.log('⚽ Ready to scrape football data!')
 
 program.action(async () => {
-  const { action, competition } = await inquirer.prompt([
+  const { mainAction } = await inquirer.prompt([
     {
-      type: 'list',
-      choices: Object.values(Actions),
-      name: 'action',
-      message: 'What you want to scrape'
-    },
-    {
-      type: 'list',
-      choices: LEAGUES_AVAILABLE.map((league) => league.name),
-      name: 'competition',
-      message: `Which competition do you want to scrape?`
+      type: 'select',
+      name: 'mainAction',
+      message: 'What do you want to do?',
+      choices: [
+        { name: 'Scrape Data', value: 'Scrape' },
+        { name: 'Insert Data into DB', value: 'Insert' }
+      ]
     }
   ])
 
-  if (action === Actions.Matches) {
-    const answers = await inquirer.prompt([
+  if (mainAction === 'Scrape') {
+    const { action, competition } = await inquirer.prompt([
       {
         type: 'list',
-        choices: ['2022-2023', '2023-2024', '2024-2025'],
-        name: 'season',
-        message: 'Which season do you want to scrape?'
+        choices: Object.values(Actions),
+        name: 'action',
+        message: 'What you want to scrape'
       },
       {
-        type: 'input',
-        name: 'rounds',
-        message: 'Which rounds do you want to scrape? (e.g. 1-6 or 2)',
-        default: 'All'
+        type: 'list',
+        choices: LEAGUES_AVAILABLE.map((league) => league.name),
+        name: 'competition',
+        message: `Which competition do you want to scrape?`
       }
     ])
-    const parsed = parseAnswers({ ...answers, competition })
-    await Commands.ScrapeMatches({ ...parsed })
+
+    if (action === Actions.Matches) {
+      const answers = await inquirer.prompt([
+        {
+          type: 'list',
+          choices: ['2022-2023', '2023-2024', '2024-2025'],
+          name: 'season',
+          message: 'Which season do you want to scrape?'
+        },
+        {
+          type: 'input',
+          name: 'rounds',
+          message: 'Which rounds do you want to scrape? (e.g. 1-6 or 2)',
+          default: 'All'
+        }
+      ])
+      const parsed = parseAnswers({ ...answers, competition })
+      await Commands.ScrapeMatches({ ...parsed })
+    }
+
+    if (action === Actions.Teams) {
+      const league = LEAGUES_AVAILABLE.find(
+        (league) => league.name === competition
+      )
+      if (!league) throw new Error('League not found')
+      await Commands.ScrapeTeams({
+        acrom: league.acrom,
+        id: league.id,
+        name: league.name
+      })
+    }
   }
-  const league = LEAGUES_AVAILABLE.find((league) => league.name === competition)
-  if (!league) throw new Error('League not found')
-  await Commands.ScrapeTeams({
-    acrom: league.acrom,
-    id: league.id,
-    name: league.name
-  })
+
+  if (mainAction === 'Insert') {
+  }
 })
 
 program.parseAsync()
